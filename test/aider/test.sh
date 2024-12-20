4caf606fc392af8f87be7fe41dc652b3f7ade3b3
Ivy Evans <ivy@ivyevans.net>
2024-12-20 07:21:19 +0000

    aider: Add tests for Playwright install

--- test/aider/test.sh
+++ test/aider/test.sh
@@ -42,6 +42,7 @@ source dev-container-features-test-lib
 # The 'check' command comes from the dev-container-features-test-lib. Syntax is...
 # check <LABEL> <cmd> [args...]
 check "validate aider installed" command -v aider
+check "validate playwright installed" command -v playwright
 
 # Report result
 # If any of the checks above exited with a non-zero exit code, the test will fail.
---
+++ test/aider/without_playwright.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# This test file will be executed against one of the scenarios devcontainer.json test that
+# includes the 'color' feature with "favorite": "gold" option.
+
+set -e
+
+# Optional: Import test library bundled with the devcontainer CLI
+# shellcheck disable=SC1091
+source dev-container-features-test-lib
+
+# Feature-specific tests
+# The 'check' command comes from the dev-container-features-test-lib.
+# shellcheck disable=SC2016
+check "playwright is not installed" bash -c '! command -v playwright'
+
+# Report result
+# If any of the checks above exited with a non-zero exit code, the test will fail.
+reportResults
